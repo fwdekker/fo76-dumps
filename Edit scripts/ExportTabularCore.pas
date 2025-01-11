ed5185649d5c02fd56da5dc2a6df545f21dae831
Florine W. Dekker <florine@fwdekker.com>
2025-01-11 14:56:54 +0100

    Escape newlines in CSV

--- Edit scripts/ExportTabularCore.pas
+++ Edit scripts/ExportTabularCore.pas
@@ -5,14 +5,18 @@ unit ExportTabularCore;
 
 
 (**
- * Escapes [text] by escaping quotes and then surrounding it with quotes.
+ * Escapes [text] by escaping newlines and quotes and then surrounding it with quotes.
  *
  * @param text  the text to escape
  * @return a CSV-escaped version of [text]
  *)
 function escapeCsvString(text: String): String;
 begin
-    result := '"' + stringReplace(text, '"', '""', [rfReplaceAll]) + '"';
+    result := text;
+    result := stringReplace(result, #13 + #10, '\n', [rfReplaceAll]);
+    result := stringReplace(result, #10, '\n', [rfReplaceAll]);
+    result := stringReplace(result, '"', '""', [rfReplaceAll]);
+    result := '"' + result + '"';
 end;
 
 
