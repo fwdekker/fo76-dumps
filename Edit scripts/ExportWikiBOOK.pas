042ea6ee2dcc7712ba3d7e2350dcfe093cf9e7e5
Florine W. Dekker <florine@fwdekker.com>
2025-01-13 17:34:04 +0100

    Remove unnecessary extraction scripts
    This reverts commit 0a7b94ab66b5b233a3101a94103af8fa0137aa02.
    This reverts commit 3d795e07a26c32c55ecb457a3e062292fb4914fe.
    
    The person who requested the additional dumps (including LSCR) indicated that they do not need automatic dumping of the Wizard's Fortunes and the perk Funnies, only the loading screens. Therefore, partial attempts at implementing those have been removed, and only LSCR dumps remain.

--- Edit scripts/ExportWikiBOOK.pas
+++ Edit scripts/ExportWikiBOOK.pas
@@ -1,19 +1,14 @@
 unit ExportWikiBOOK;
 
 uses ExportCore,
-     ExportTabularCore,
      ExportWikiCore;
 
 
-var ExportTabularFORTUNE_outputLines: TStringList;
 var ExportWikiBOOK_outputLines: TStringList;
 
 
 function initialize(): Integer;
 begin
-    ExportTabularFORTUNE_outputLines := TStringList.create();
-    ExportTabularFORTUNE_outputLines.add('"Name", "Fortune"');
-
     ExportWikiBOOK_outputLines := TStringList.create();
 end;
 
@@ -25,35 +20,24 @@ begin
 end;
 
 function _process(book: IInterface): Integer;
-var edid: String;
-    full: String;
-    desc: String;
 begin
-    edid := getEditValue(elementBySignature(book, 'EDID'));
-    full := getEditValue(elementBySignature(book, 'FULL'));
-    desc := trim(getEditValue(elementBySignature(book, 'DESC')));
-
-    if strEquals(leftStr(edid, 17), 'ATX_Book_Fortunes') then begin
-        ExportTabularFORTUNE_outputLines.add(escapeCsvString(full) + ', ' + stripHtmlTags(escapeCsvString(desc)));
-    end;
-
-    ExportWikiBOOK_outputLines.add('==[' + getFileName(getFile(book)) + '] ' + full + '==');
+    ExportWikiBOOK_outputLines.add(
+        '==[' + getFileName(getFile(book)) + '] ' +
+        getEditValue(elementBySignature(book, 'FULL')) +
+        '=='
+    );
     ExportWikiBOOK_outputLines.add('Form ID:      ' + stringFormID(book));
-    ExportWikiBOOK_outputLines.add('Editor ID:    ' + edid);
+    ExportWikiBOOK_outputLines.add('Editor ID:    ' + getEditValue(elementBySignature(book, 'EDID')));
     ExportWikiBOOK_outputLines.add('Weight:       ' + getEditValue(elementByPath(book, 'DATA\Weight')));
     ExportWikiBOOK_outputLines.add('Value:        ' + getEditValue(elementByPath(book, 'DATA\Value')));
     ExportWikiBOOK_outputLines.add('Can be taken: ' + canBeTakenString(book));
-    ExportWikiBOOK_outputLines.add('Transcript:' + #10 + asWikiTranscript(desc));
+    ExportWikiBOOK_outputLines.add('Transcript:' + #10 + getBookContents(book));
     ExportWikiBOOK_outputLines.add(#10);
 end;
 
 function finalize(): Integer;
 begin
     createDir('dumps/');
-
-    ExportTabularFORTUNE_outputLines.saveToFile('dumps/FORTUNE.csv');
-    ExportTabularFORTUNE_outputLines.free();
-
     ExportWikiBOOK_outputLines.saveToFile('dumps/BOOK.wiki');
     ExportWikiBOOK_outputLines.free();
 end;
@@ -76,40 +60,15 @@ begin
     end;
 end;
 
-function stripHtmlTags(text: String): String;
-var remainder: String;
-    remainder2: String;
+function getBookContents(book: IInterface): String;
+var desc: String;
 begin
-    addMessage(text);
-    { Note: This method does not work with nested HTML tags! }
-    { TODO: This function currently gets stuck in an infinite loop, unfortunately... }
+    desc := trim(escapeWiki(getEditValue(elementBySignature(book, 'DESC'))));
 
-    remainder := text;
-    result := '';
-
-    while strLen(remainder) > 0 do begin
-        if strLComp(remainder, '<', 1) = 0 then begin
-            remainder := strScan(remainder, '>');
-            delete(remainder, 1, 1);
-        end else begin
-            remainder2 := strScan(remainder, '<');
-            if strLen(remainder2) = 0 then begin
-                result := strCat(result, remainder);
-                break;
-            end else begin
-                result := result + leftStr(remainder, strLen(remainder) - strLen(remainder2));
-                remainder := remainder2;
-            end;
-        end;
-    end;
-end;
-
-function asWikiTranscript(text: String): String;
-begin
-    if text = '' then begin
+    if desc = '' then begin
         result := 'No transcript';
     end else begin
-        result := '{{Transcript|text=' + #10 + escapeWiki(text) + #10 + '}}';
+        result := '{{Transcript|text=' + #10 + desc + #10 + '}}';
     end;
 end;
 
